1ca4a8a4e4c8ff61ddb828618f405eafc4bc3f6e
Fernando <fernandoquevedo2198@gmail.com>
2023-11-17 08:23:53 -0500

    fixed chart

---
+++ src/containers/Investigate/Vulnerabilities/TimelineChart.jsx
@@ -0,0 +1,36 @@
+const data = [
+  {
+    date: "04/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+  {
+    date: "05/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+  {
+    date: "06/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+  {
+    date: "07/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+  {
+    date: "08/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+  {
+    date: "09/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+  {
+    date: "10/09",
+    value: { in_progress: 10000, mitigated: 10000, new: 5000, rejected: 7000 },
+  },
+];
+
+const TimelineChart = () => {
+  return <div></div>;
+};
+
+export default TimelineChart;
