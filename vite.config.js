25f096d70c9ae7e9456bd8d0ccc5c2187b29e1d3
Fernando <fernandoquevedo2198@gmail.com>
2023-11-20 21:27:07 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["user-debounce"],
+    },
+  },
   plugins: [
     react(),
     svgr({
