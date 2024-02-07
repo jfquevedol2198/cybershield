936b60c48ba299b9ee006877ecd71fc69b7ed121
Fernando <fernandoquevedo2198@gmail.com>
2024-02-07 10:48:24 -0500

    remove mitigate on EventCard

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -2,11 +2,10 @@ import clsx from "clsx";
 import PropTypes from "prop-types";
 
 import { RiskLevel, getRiskLevel } from "../../utils/risk";
-import NormalButton from "../NormalButton";
 
 const EventCard = ({ title, description, score }) => {
   return (
-    <div className="shadow-dropdown mx-2 my-2 flex flex-row gap-2 p-2">
+    <div className="mx-2 my-2 flex flex-row gap-2 p-2 shadow-dropdown">
       <span
         className={clsx(
           "flex h-auto w-2 flex-[0_0_8px] rounded-full",
@@ -15,20 +14,15 @@ const EventCard = ({ title, description, score }) => {
       />
       <div className="flex flex-auto flex-col justify-between gap-2 px-2 py-2">
         {title && (
-          <div className="text-gray-5 text-[1.375rem] font-bold">{title}</div>
+          <div className="text-[1.375rem] font-bold text-gray-5">{title}</div>
         )}
         {description && (
-          <div className="text-gray-5 text-base font-normal">{description}</div>
+          <div className="text-base font-normal text-gray-5">{description}</div>
         )}
-        <NormalButton
-          className="text-primary-4"
-          label="Mitigate"
-          onClick={() => alert("Mitigate")}
-        />
       </div>
       <div className="flex flex-col justify-center text-center">
-        <span className="text-gray-5 text-sm font-normal">Risk score</span>
-        <span className="text-gray-5 text-[2.5rem] font-light">{score}%</span>
+        <span className="text-sm font-normal text-gray-5">Risk score</span>
+        <span className="text-[2.5rem] font-light text-gray-5">{score}%</span>
       </div>
     </div>
   );
